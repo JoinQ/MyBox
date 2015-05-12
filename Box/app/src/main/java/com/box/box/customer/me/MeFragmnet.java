66f54cffd8f46894fc701744f3ee9af3fe2e9ee5
无名仕 <2469763120@qq.com>
2015-05-13 07:15:03 +0800

    个人中心

--- Box/app/src/main/java/com/box/box/customer/me/MeFragmnet.java
+++ Box/app/src/main/java/com/box/box/customer/me/MeFragmnet.java
@@ -1,103 +1,143 @@
 package com.box.box.customer.me;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import com.box.box.R;
 import com.box.box.customer.me.function.MyInformationActivity;
 import com.box.box.customer.me.function.ShareDialogFragment;
-import com.box.util.Utils;
+import com.box.mode.MeList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MeFragmnet extends Fragment implements View.OnClickListener{
-	private LinearLayout sign;
-	private LinearLayout myInfoBtn;
-	private LinearLayout rewardBtn;
-	private LinearLayout logisticsBtn;
-	private LinearLayout appBtn;
-	private LinearLayout rankBtn;
-	private LinearLayout shareBtn;
-	private LinearLayout fbBtn;
-	private LinearLayout vipBtn;
-	private LinearLayout setBtn;
+public class MeFragmnet extends Fragment implements View.OnClickListener, ListView.OnItemClickListener {
+    private ListView listView;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		ActionBar actionBar = getActivity().getActionBar();
-		actionBar.setDisplayShowHomeEnabled(false);
-		actionBar.setTitle("个人中心");
-	}
+    private List<MeList> list = new ArrayList<MeList>();
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View root = inflater.inflate(R.layout.fragment_me, container, false);
-		initView(root);
-		return root;
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        list.add(new MeList(R.drawable.me_info, "个人信息", true));
+        list.add(new MeList(R.drawable.me_logistics, "我的物流", true));
+        list.add(new MeList(R.drawable.me_application, "应用中心", false));
+        list.add(new MeList(R.drawable.me_share, "向好友推荐", false));
+        list.add(new MeList(R.drawable.me_help, "帮助", true));
+        list.add(new MeList(R.drawable.me_rank, "排名", false));
+        list.add(new MeList(R.drawable.me_set, "设置", false));
+        ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setTitle("个人中心");
+    }
 
-	private void initView(View root) {
-		sign = (LinearLayout) root.findViewById(R.id.me_sign);
-		myInfoBtn = (LinearLayout) root.findViewById(R.id.me_tab_myinfo);
-		rewardBtn = (LinearLayout) root.findViewById(R.id.me_tab_reward);
-		logisticsBtn = (LinearLayout) root.findViewById(R.id.me_tab_logistics);
-		appBtn = (LinearLayout) root.findViewById(R.id.me_tab_application);
-		rankBtn = (LinearLayout) root.findViewById(R.id.me_tab_rank);
-		shareBtn = (LinearLayout) root.findViewById(R.id.me_tab_share);
-		fbBtn = (LinearLayout) root.findViewById(R.id.me_tab_help);
-		vipBtn = (LinearLayout) root.findViewById(R.id.me_tab_vip);
-		setBtn = (LinearLayout) root.findViewById(R.id.me_tab_set);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_me, container, false);
+        initView(root);
+        return root;
+    }
 
-		sign.setOnClickListener(this);
-		myInfoBtn.setOnClickListener(this);
-		rewardBtn.setOnClickListener(this);
-		logisticsBtn.setOnClickListener(this);
-		appBtn.setOnClickListener(this);
-		rankBtn.setOnClickListener(this);
-		shareBtn.setOnClickListener(this);
-		fbBtn.setOnClickListener(this);
-		vipBtn.setOnClickListener(this);
-		setBtn.setOnClickListener(this);
-	}
+    private void initView(View root) {
+        listView = (ListView) root.findViewById(R.id.me_list);
+        listView.setAdapter(new MyAdapter(list, getActivity()));
+        listView.setOnItemClickListener(this);
+    }
 
-	@Override
-	public void onClick(View view) {
-		switch (view.getId()) {
-			case R.id.me_sign:
-				break;
-			case R.id.me_tab_myinfo:
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (i) {
+            case 0:
                 startActivity(new Intent(MeFragmnet.this.getActivity(), MyInformationActivity.class));
-				break;
-			case R.id.me_tab_reward:
-				break;
-			case R.id.me_tab_logistics:
-				break;
-			case R.id.me_tab_application:
-				break;
-			case R.id.me_tab_rank:
-				break;
-			case R.id.me_tab_share:
-				Utils.Toast("share");
-				new ShareDialogFragment().show(getActivity().getFragmentManager(), "share");
-				break;
-			case R.id.me_tab_help:
-				break;
-			case R.id.me_tab_vip:
-				break;
-			case R.id.me_tab_set:
-				break;
-		}
-	}
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                new ShareDialogFragment().show(getActivity().getFragmentManager(), "share");
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    class MyAdapter extends BaseAdapter {
+        private List<MeList> meList;
+        private Context context;
+
+        public MyAdapter(List<MeList> meList, Context context) {
+            this.meList = meList;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return meList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return meList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View rootView;
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                rootView = LayoutInflater.from(context).inflate(R.layout.list_me_adapter, parent, false);
+                viewHolder.mView = (View) rootView.findViewById(R.id.me_adapter_view);
+                viewHolder.mNameText = (TextView) rootView.findViewById(R.id.me_adapter_name);
+                viewHolder.mHeadImage = (ImageView) rootView.findViewById(R.id.me_adapter_icon);
+                rootView.setTag(viewHolder);
+            } else {
+                rootView = convertView;
+                viewHolder = (ViewHolder) rootView.getTag();
+            }
+            viewHolder.mNameText.setText(meList.get(position).getName());
+            viewHolder.mHeadImage.setImageResource(meList.get(position).getImageId());
+            if (!meList.get(position).getIsAppear()) {
+                viewHolder.mView.setVisibility(View.GONE);
+            }
+            return rootView;
+        }
+
+        private class ViewHolder {
+            private View mView;
+            private TextView mNameText;
+            private ImageView mHeadImage;
+        }
+    }
 }
---
+++ Box/app/src/main/java/com/box/mode/MeList.java
@@ -0,0 +1,37 @@
+package com.box.mode;
+
+public class MeList {
+    private int imageId;
+    private String name;
+    private Boolean isAppear;
+
+    public MeList(int imageId, String name, Boolean isAppear) {
+        this.imageId = imageId;
+        this.isAppear = isAppear;
+        this.name = name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getIsAppear() {
+        return isAppear;
+    }
+
+    public void setIsAppear(Boolean isAppear) {
+        this.isAppear = isAppear;
+    }
+}
