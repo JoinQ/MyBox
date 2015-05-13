c5c1ad01d4a6e1b9a327dff7738408e96dc22b87
yyx290799684 <yyx290799684@hotmail.com>
2015-05-13 17:11:29 +0800

    用户_个人信息V1.1

--- Box/app/src/main/java/com/box/box/customer/me/function/MyInformationActivity.java
+++ Box/app/src/main/java/com/box/box/customer/me/function/MyInformationActivity.java
@@ -1,12 +1,15 @@
 package com.box.box.customer.me.function;
 
+import android.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.box.app.BoxActivity;
 import com.box.box.R;
+import com.box.util.Utils;
 
 public class MyInformationActivity extends BoxActivity {
 
@@ -14,7 +17,36 @@ public class MyInformationActivity extends BoxActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_information);
+
+        ActionBar actionBar = this.getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setTitle("个人信息");
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.action_editting:
+                Utils.Toast("编辑");
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_my_information, menu);
+        return true;
+    }
+
 
 
 }
