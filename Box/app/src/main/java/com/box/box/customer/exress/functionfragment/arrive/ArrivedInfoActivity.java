e8831ee6f994b98960b7e1676318a68c9b5af095
yyx290799684 <yyx290799684@hotmail.com>
2015-05-26 17:14:05 +0800

    取件密码 V1.0

--- Box/app/src/main/java/com/box/box/customer/exress/functionfragment/arrive/ArrivedInfoActivity.java
+++ Box/app/src/main/java/com/box/box/customer/exress/functionfragment/arrive/ArrivedInfoActivity.java
@@ -1,13 +1,20 @@
 package com.box.box.customer.exress.functionfragment.arrive;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.box.app.BoxActivity;
 import com.box.box.R;
+import com.box.box.customer.MainActivity;
 
-public class ArrivedInfoActivity extends BoxActivity {
+public class ArrivedInfoActivity extends BoxActivity implements View.OnClickListener {
+
+    private TextView arr_not_rec_tv_exception;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +29,8 @@ public class ArrivedInfoActivity extends BoxActivity {
     }
 
     private void init() {
-
+        arr_not_rec_tv_exception = (TextView) findViewById(R.id.arr_not_rec_tv_exception);
+        arr_not_rec_tv_exception.setOnClickListener(this);
     }
 
     @Override
@@ -35,5 +43,19 @@ public class ArrivedInfoActivity extends BoxActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.arr_not_rec_tv_exception:
+                new AlertDialog.Builder(ArrivedInfoActivity.this).setTitle("取件遇到问题？")//设置对话框标题
+                        .setMessage("请用以下密码取件："+"123456")//设置显示的内容
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {//添加确定按钮
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {//确定按钮的响应事件
+                            }
+                        }).show();
+        }
     }
 }
